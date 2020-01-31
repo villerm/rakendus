7c0649d37ed639391b4b52d005533af133066beb
Viller Maine <viller.maine@gmail.com>
2020-01-31 11:59:13 +0200

    FEATURE: Added partofday

--- page.php
+++ page.php
@@ -1,3 +1,18 @@
+<?php 
+    $myName = "Viller Maine";
+    $fullTimeNow = date("d.m.Y H:i:s");
+    $hourNow = date("H");
+    $timeHTML = "<p>Lehe avamise hetkel oli: <strong>".$fullTimeNow."<strong></p>";
+    $partOfDay = "Hägunea aeg";
+
+    if($hourNow < 10):
+        $partOfDay = "hommik";
+    elseif( $hourNow >= 10 && $hourNow < 18 ):
+        $partOfDay = "aeg õppida!";
+    else:
+        $partOfDay = "puhka nüüd!";
+    endif;
+?>
 <html lang="et">
 <head>
     <meta charset="UTF-8">
@@ -6,6 +21,8 @@
     <title>Veebirakendus | Viller Maine</title>
 </head>
 <body>
-    
+    <h1><?php echo $myName;?></h1>
+    <?php echo $timeHTML; ?>
+    <?php echo $partOfDay; ?>
 </body>
 </html>
