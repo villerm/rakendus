ed7d0c6da565504b21f0f11e816163d55cb4258a
Viller Maine <viller.maine@gmail.com>
2020-02-27 12:34:36 +0200

    FEATURE: added functions

--- 2tund/functions.php
+++ 2tund/functions.php
@@ -1,4 +1,24 @@
-<?php 
-
-
-?>
+<?php
+function saveNews($userid, $title, $content)
+{
+    $response = null;
+    //andmebaasi yhendus
+    $conn = new mysqli($GLOBALS['serverHost'], $GLOBALS['serverUserName'], $GLOBALS['serverPassword'], $GLOBALS['database']);
+    //sql paring
+    $stmt = $conn->prepare('INSERT INTO vr_news (userid, title, content) VALUES (?, ?, ?)');
+    echo $conn->error;
+    //annan paringule paris andmed
+    $userid = 1;
+    // i=int s=string d=decimal
+    $stmt->bind_param("iss", $userid, $title, $content);
+    if ($stmt->execute()) {
+        $response = 1;
+    } else {
+        $response = 0;
+        echo $stmt->error;
+    }
+    //sulgen paringu ja andmebaasi yhenduse
+    $stmt->close();
+    $conn->close();
+    return $response;
+}
