f107dd9e25ccfc6729b26703c896a20a58f47ea0
Viller Maine <viller.maine@gmail.com>
2020-05-16 18:25:05 +0300

    CHANGE: Url fix

--- header.php
+++ header.php
@@ -38,7 +38,7 @@ if(isset($_POST["login"])){
 </head>
 <div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="http://localhost/Kool/Veebirakendus/rakendus/">Veebirakendused</a>
+  <a class="navbar-brand" href=".">Veebirakendused</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
