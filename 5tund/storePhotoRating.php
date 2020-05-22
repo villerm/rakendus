d3862a97f254c2b01b034cfb79b0e61f4456e293
Viller Maine <viller.maine@gmail.com>
2020-05-22 16:51:41 +0300

    session fix

--- 5tund/storePhotoRating.php
+++ 5tund/storePhotoRating.php
@@ -1,9 +1,12 @@
 <?php
     $id = $_REQUEST["photoId"];
     $rating = $_REQUEST["rating"];
+    require("../classes/Session.class.php");
+    SessionManager::sessionStart("vr20", 0, "/~viller.maine/", "tigu.hk.tlu.ee");
     require('../../../../config.php');
     $database = 'villermaine';
     $conn = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUserName"], $GLOBALS["serverPassword"], $database);
+    $firstError = $conn->error;
     $stmt = $conn->prepare("INSERT INTO vr20_photoratings (photoid, userid, rating) VALUES (?, ?, ?)");
     $stmt->bind_param("iii", $id, $_SESSION["userid"], $rating);
     $stmt->execute();
